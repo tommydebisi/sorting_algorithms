447361ba1e5e61570c7094ab08cac3a2303dea56
tommydebisi <tommydebisi@gmail.com>
2022-07-16 00:35:03 +0100

    :construction: task 1-3

--- sort.h
+++ sort.h
@@ -23,5 +23,8 @@ void print_list(const listint_t *list);
 
 /* Sort Algorithms */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H_ */

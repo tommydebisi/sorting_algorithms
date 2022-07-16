229599e2ef3b76c8a703dc156fc3c1caceac19d3
Yahaya <azconcept2016@gmail.com>
2022-07-16 20:00:18 +0100

    :seedling: update file

--- sort.h
+++ sort.h
@@ -21,10 +21,12 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sort Algorithms */
+/* Sort Algorithms prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
 
 #endif /* SORT_H_ */

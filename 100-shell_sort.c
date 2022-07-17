8fc431b28d6fe643e7eac3cf847bc3d383b47a0e
Yahaya <azconcept2016@gmail.com>
2022-07-17 18:54:01 +0100

    :recycle: refactor code

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,7 +7,7 @@
  *
  * Return: the power of x
  */
-int find_pow(int x, int y)
+int find_pow(int x, size_t y)
 {
 	if (y == 0)
 		return (1);
@@ -33,32 +33,40 @@ int *seq_generator(size_t size)
 	nth_term = 0;
 	while (n < size)
 	{
-		nth_term = nth_term + find_pow(3, i);
+		nth_term = nth_term + find_pow(3, n);
 		sequence[i] = nth_term;
 		n++;
 		i++;
 	}
-
 	return (sequence);
 }
 
 /**
- * find_seq_length - find length of sequence
+ * reverse_seq - reverse sequence
  * @sequence: pointer to the address of sequence
+ * @size: size of sequence
  *
- * Return: length of sequence
+ * Return: sequence in reverse
  */
-int find_seq_length(int *sequence)
+int *reverse_seq(int *sequence, size_t size)
 {
-	int count = 0, i = 0;
+	int *rev_seq;
+	size_t i = 0, seq_index;
 
-	while (sequence[i])
+	rev_seq = malloc(sizeof(int) * size);
+	if (rev_seq == NULL)
+		return (NULL);
+
+	seq_index = size - 1;
+	while (i < size)
 	{
-		count++;
+		rev_seq[i] = sequence[seq_index];
 		i++;
+		seq_index--;
 	}
-	return (count);
+	return (rev_seq);
 }
+
 /**
  * shell_sort - sorts using the Shell sort algorithm
  * @array: array to be sorted
@@ -68,30 +76,36 @@ int find_seq_length(int *sequence)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp, seq_size, *sequence, seq_index = 0;
-
-	if (size == 2)
-		return;
+	int j, flag = 0;
+	int temp, *sequence, *rev_seq, hold;
+	size_t i, seq_index = 0;
 
 	sequence = seq_generator(size);
 	if (sequence == NULL)
 		return;
-	seq_size = find_seq_length(sequence);
 
-	while (seq_index < seq_size)
+	rev_seq = reverse_seq(sequence, size);
+	if (rev_seq == NULL)
+		return;
+
+	while (seq_index < size)
 	{
-		for (i = sequence[seq_index]; i < size; i++)
+		/*printf("sequence[%li] = %i\n", seq_index, rev_sequence[seq_index]);*/
+		for (i = rev_seq[seq_index]; i < size; i++)
 		{
 			temp = array[i];
-			for (j = i; array[j - sequence[seq_index]] > temp; j -= sequence[seq_index])
+			hold = rev_seq[seq_index];
+			for (j = i; j >= hold && array[j - hold] > temp; j -= hold)
 			{
-				array[j] = array[j - sequence[seq_index]];
+				array[j] = array[j - rev_seq[seq_index]];
 			}
 			array[j] = temp;
-			print_array(array, size);
+			flag = 1;
 		}
+		if (flag)
+			print_array(array, size);
 		seq_index++;
 	}
 	free(sequence);
+	free(rev_seq);
 }

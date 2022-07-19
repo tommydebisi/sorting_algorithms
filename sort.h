6e0e3234aab92637aa8416db53c82861402fb66b
tommydebisi <tommydebisi@gmail.com>
2022-07-19 23:06:22 +0100

    :construction: bitonic and quick hoarse sort

--- sort.h
+++ sort.h
@@ -31,5 +31,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H_ */
